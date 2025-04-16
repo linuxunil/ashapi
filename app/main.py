c0079803093a7fac23693714edb3615b84083228
linuxunil <linuxunil@gmail.com>
2025-04-16 10:53:54 -0500

    Serving static files

--- app/main.py
+++ app/main.py
@@ -1,12 +1,24 @@
 from fastapi import FastAPI
-from app.models import Pokemon
+from fastapi.staticfiles import StaticFiles
 from app.database import create_db_and_tables
 from app.routers import router
 from app.scripts import populate_db
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-app.include_router(router)
+origins = [
+    "http://localhost:5173",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+app.mount("/sprite", StaticFiles(directory="/code/app/static"), name="static")
+app.include_router(router)
 
 @app.on_event("startup")
 def on_startup():
